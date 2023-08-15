e374d3bdce3eeeb665df7efe9ac1cb38ddf8cd2b
Saif Metkari <88455816+Wiz17@users.noreply.github.com>
2023-08-15 13:35:47 +0530

    Update scriptc.js

--- scriptc.js
+++ scriptc.js
@@ -26,3 +26,11 @@ window.addEventListener("scroll", function () {
 
     }
   });
+
+ function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
